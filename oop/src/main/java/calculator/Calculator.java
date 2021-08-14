ef1f5698f4ad51793f6f2a2f15966cb1b0ab7309
soheeoott <soheeoott@gmail.com>
2021-08-15 00:25:52 +0900

    Refactor : Maven 프로젝트로 변경, indexOf → switch문으로 변경

---
+++ oop/src/main/java/calculator/Calculator.java
@@ -0,0 +1,39 @@
+package calculator;
+
+import java.io.*;
+
+public class Calculator {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] numArr = br.readLine().split(" ");
+
+        int result = Integer.parseInt(numArr[0]);
+
+        for (int i = 1; i < numArr.length; i += 2) {
+
+            int num = Integer.parseInt(numArr[i + 1]);
+
+            switch (numArr[i]) {
+                case "+":
+                    result += num;
+                    break;
+                case "-":
+                    result -= num;
+                    break;
+                case "/":
+                    result /= num;
+                    break;
+                case "*":
+                    result *= num;
+                    break;
+            }
+        }
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
