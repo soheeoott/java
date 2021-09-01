53895d637d6f76ded62043d405af07bcd30da5a2
soheeoott <soheeoott@gmail.com>
2021-09-01 23:24:28 +0900

    Refactor : 문자열 상수로 수정 및 메소드 분리

--- TDD/src/main/java/racing/InputView.java
+++ TDD/src/main/java/racing/InputView.java
@@ -4,32 +4,31 @@ import java.util.Scanner;
 
 public class InputView {
 
-    public int carQuantity = 0;
-    public int tryCount = 0;
+    private static final String MESSAGE_USER_INPUT_CAR_QUANTITY = "자동차 대수는 몇 대 인가요?";
+    private static final String MESSAGE_USER_INPUT_TRY_COUNT = "시도할 회수는 몇 회 인가요?";
 
-    public void inputValue() {
-        Scanner sc = new Scanner(System.in);
+    private int setUpCarQuantity (Scanner scanner) throws Exception {
+        System.out.println(MESSAGE_USER_INPUT_CAR_QUANTITY);
 
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        inputCarQuantity(sc.nextInt());
-
-        System.out.println("시도할 회수는 몇 회 인가요?");
-        inputTryCount(sc.nextInt());
+//        if (scanner.nextInt() > 0 || scanner.nextInt() > 9) { // 메소드 ?
+//            throw new Exception("1~9 범위 이내의 숫자를 입력해주세요.");
+//        }
+        return scanner.nextInt();
     }
 
-    void inputCarQuantity(int carQuantity) {
-        this.carQuantity = carQuantity;
+    // 10회로 한정
+    private int setUpTryCount (Scanner scanner) throws Exception {
+        System.out.println(MESSAGE_USER_INPUT_TRY_COUNT);
+
+//        if (scanner.nextInt() > 0 || scanner.nextInt() > 9) { // 메소드 ?
+//            throw new Exception("1~9 범위 이내의 숫자를 입력해주세요.");
+//        }
+        return scanner.nextInt();
     }
 
-    void inputTryCount(int tryCount) {
-        this.tryCount = tryCount;
-    }
-
-    int getCarQuantity() {
-        return carQuantity;
-    }
-
-    int getTryCount() {
-        return tryCount;
+    public void setUpRacingGame () throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int carQuantity = setUpCarQuantity(scanner);
+        int tryCount = setUpTryCount(scanner);
     }
 }
