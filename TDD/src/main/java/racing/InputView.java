b6528fae2549de39250cab4e9942aef4c74ebe14
soheeoott <soheeoott@gmail.com>
2021-09-02 00:13:50 +0900

    Refactor : 입력 클래스, 조건식 메소드로 변경

--- TDD/src/main/java/racing/InputView.java
+++ TDD/src/main/java/racing/InputView.java
@@ -4,31 +4,36 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private static final String MESSAGE_USER_INPUT_CAR_QUANTITY = "자동차 대수는 몇 대 인가요?";
-    private static final String MESSAGE_USER_INPUT_TRY_COUNT = "시도할 회수는 몇 회 인가요?";
+    private static final String INPUT_MESSAGE_CAR_QUANTITY = "자동차 대수는 몇 대 인가요?";
+    private static final String INPUT_MESSAGE_TRY_COUNT = "시도할 회수는 몇 회 인가요?";
+    private static final String ERROR_MESSAGE_INPUT_RANGE = "1 ~ 9까지의 숫자를 입력해주세요.";
 
-    private int setUpCarQuantity (Scanner scanner) throws Exception {
-        System.out.println(MESSAGE_USER_INPUT_CAR_QUANTITY);
+    private Scanner scanner;
 
-//        if (scanner.nextInt() > 0 || scanner.nextInt() > 9) { // 메소드 ?
-//            throw new Exception("1~9 범위 이내의 숫자를 입력해주세요.");
-//        }
+    public InputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    Boolean setUpRacingInputCheck () {
+        return scanner.nextInt() < 1 || scanner.nextInt() > 9; // 입력 범위 : 1~9
+    }
+
+    private int setUpCarQuantity () throws Exception {
+        System.out.println(INPUT_MESSAGE_CAR_QUANTITY);
+
+        if (setUpRacingInputCheck()) {
+            throw new Exception(ERROR_MESSAGE_INPUT_RANGE);
+        }
         return scanner.nextInt();
     }
 
     // 10회로 한정
-    private int setUpTryCount (Scanner scanner) throws Exception {
-        System.out.println(MESSAGE_USER_INPUT_TRY_COUNT);
+    private int setUpTryCount () throws Exception {
+        System.out.println(INPUT_MESSAGE_TRY_COUNT);
 
-//        if (scanner.nextInt() > 0 || scanner.nextInt() > 9) { // 메소드 ?
-//            throw new Exception("1~9 범위 이내의 숫자를 입력해주세요.");
-//        }
+        if (setUpRacingInputCheck()) {
+            throw new Exception(ERROR_MESSAGE_INPUT_RANGE);
+        }
         return scanner.nextInt();
-    }
-
-    public void setUpRacingGame () throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        int carQuantity = setUpCarQuantity(scanner);
-        int tryCount = setUpTryCount(scanner);
     }
 }
