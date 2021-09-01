cf114778982febbfcdf7581343aab6af8007a174
soheeoott <soheeoott@gmail.com>
2021-09-02 01:19:18 +0900

    Refactor : 입력 클래스, 매직 넘버 수정

--- TDD/src/main/java/racing/InputView.java
+++ TDD/src/main/java/racing/InputView.java
@@ -6,7 +6,10 @@ public class InputView {
 
     private static final String INPUT_MESSAGE_CAR_QUANTITY = "자동차 대수는 몇 대 인가요?";
     private static final String INPUT_MESSAGE_TRY_COUNT = "시도할 회수는 몇 회 인가요?";
-    private static final String ERROR_MESSAGE_INPUT_RANGE = "1 ~ 9까지의 숫자를 입력해주세요.";
+    private static final String ERROR_MESSAGE_INPUT_RANGE = "1부터 9까지 범위 이내의 숫자를 입력해주세요.";
+
+    private static final int INPUT_MIN_RANGE_NUMBER = 1;
+    private static final int INPUT_MAX_RANGE_NUMBER = 9;
 
     private Scanner scanner;
 
@@ -15,12 +18,21 @@ public class InputView {
     }
 
     Boolean setUpRacingInputCheck () {
-        return scanner.nextInt() < 1 || scanner.nextInt() > 9; // 입력 범위 : 1~9
+        return scanner.nextInt() < INPUT_MIN_RANGE_NUMBER || scanner.nextInt() > INPUT_MAX_RANGE_NUMBER;
     }
+
+    // 문자로 입력 받았을 때 => InputMismatchException
+//    Boolean setUpRacingInputNumberCheck () {
+//        try {
+//            return scanner.nextInt(); // -> 문자인지 아닌지???? Character.digit() 사용?
+//        } catch (InputMismatchException e) {
+//            System.out.println();
+//        }
+//    }
 
     private int setUpCarQuantity () throws Exception {
         System.out.println(INPUT_MESSAGE_CAR_QUANTITY);
-
+        
         if (setUpRacingInputCheck()) {
             throw new Exception(ERROR_MESSAGE_INPUT_RANGE);
         }
